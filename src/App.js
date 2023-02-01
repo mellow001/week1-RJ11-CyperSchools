32e7a838f2fc33ee61e0170b55d118fb531bc464
mellow001 <122299806+mellow001@users.noreply.github.com>
2023-02-01 19:46:11 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,24 @@
+import React from "react";
+import AllUsersList from "./components/AllUsersList";
+//import Test from "./Test";
+const App = () => {
+  return (
+    <>
+      <section>
+        <Name/>
+        
+      </section>
+    </>  
+  );
+};
+
+export default App;
+
+
+///     div
+//      /  \
+//    Test  Test
+
+///      section
+//         /  \
+//      Test   Test
